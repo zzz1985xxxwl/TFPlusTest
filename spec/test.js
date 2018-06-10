a3fab1f971c43c81158bdea73bdd2669d42a7d14
adrain.xue <adrain.xue@transfinder.com>
2018-06-10 13:31:59 +0800

    add spec

--- spec/test.js
+++ spec/test.js
@@ -11,6 +11,7 @@ describe("StopPool Data Model", function () {
 
     beforeEach(function () {
         testObj = { a: 1 };
+
         // spy part
         spyOn(testObj, "func");
         spyOnProperty(testObj, "p1");
@@ -26,6 +27,24 @@ describe("StopPool Data Model", function () {
 
     it("Test init Project", function () {
         expect(true).toEqual(true);
+        expect().nothing();
+        // https://jasmine.github.io/api/3.0/matchers.html
+        expect(thing).toBe(realThing);
+        expect(number).toBeCloseTo(42.2, 3);
+        expect(result).toBeDefined();
+        expect(result).toBeFalsy();
+        expect(result).toBeGreaterThan(3);
+        expect(result).toBeGreaterThanOrEqual(25);
+        expect(result).toBeLessThan(0);
+        expect(result).toBeUndefined();
+        expect("my string").toMatch(/string$/);
+        expect(array).toContain(anElement);
+        expect(mySpy).toHaveBeenCalled();
+        expect(mySpy).not.toHaveBeenCalled();
         expect(testObj.p1).toEqual(1);
+        expect(mySpy).toHaveBeenCalledBefore(otherSpy);
+        expect(mySpy).toHaveBeenCalledTimes(3);
+        expect(mySpy).toHaveBeenCalledWith('foo', 'bar', 2);
+        expect(function () { return 'other'; }).toThrowError();
     });
 });
